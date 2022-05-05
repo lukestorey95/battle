de691fe43b57e2a9bc166b1bf8f8b37563639ee0
Anish <anishkakaiya@outlook.com>
2022-05-05 17:50:02 +0100

    add health point functionality

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -6,8 +6,16 @@ feature Battle do
     fill_in :player_2_name, with: "Anish"
     click_button 'Submit'
 
-    save_and_open_page
+    # save_and_open_page
 
     expect(page).to have_content "Luke vs Anish"
+  end
+
+  scenario "shows player 2s HP" do
+    visit("/")
+    fill_in :player_1_name, with: "Luke"
+    fill_in :player_2_name, with: "Anish"
+    click_button 'Submit'
+    expect(page).to have_content "Anish: 60/60 HP"
   end
 end
