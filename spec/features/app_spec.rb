0aa7d9d850a4ae4096ffcbe76469b164bf296feb
Anish <anishkakaiya@outlook.com>
2022-05-05 17:55:27 +0100

    refactor

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,21 +1,13 @@
 feature Battle do
 
   scenario "check form names" do
-    visit("/")
-    fill_in :player_1_name, with: "Luke"
-    fill_in :player_2_name, with: "Anish"
-    click_button 'Submit'
-
+    sign_in_and_play
     # save_and_open_page
-
     expect(page).to have_content "Luke vs Anish"
   end
 
   scenario "shows player 2s HP" do
-    visit("/")
-    fill_in :player_1_name, with: "Luke"
-    fill_in :player_2_name, with: "Anish"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Anish: 60/60 HP"
   end
 end
